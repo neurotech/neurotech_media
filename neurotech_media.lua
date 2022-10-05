e0e6a8170fa35a8be8b7ec3555074f29e42e93da
Tim Douglas <neurotech@gmail.com>
2022-10-05 11:54:42 +1100

    Added fonts

--- neurotech_media.lua
+++ neurotech_media.lua
@@ -1,8 +1,12 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
 -- Textures
---LSM:Register("background", "TEXTURE_NAME", [[Interface\Addons\neurotech_media\textures\TEXTURE_NAME.tga]])
+LSM:Register("statusbar", "!neurotech01", [[Interface\Addons\neurotech_media\media\textures\neurotech01.tga]])
 
 -- Fonts
---LSM:Register("font", "Cabin-Bold", [[Interface\Addons\neurotech_media\fonts\Cabin-Bold.ttf]])
-LSM:Register("font", "ChronoType", [[Interface\Addons\neurotech_media\fonts\ChronoType.ttf]])
+LSM:Register("font", "Cabin-Bold", [[Interface\Addons\neurotech_media\media\fonts\Cabin-Bold.ttf]])
+LSM:Register("font", "ChronoType", [[Interface\Addons\neurotech_media\media\fonts\ChronoType.ttf]])
+LSM:Register("font", "GohuFont", [[Interface\Addons\neurotech_media\media\fonts\gohufont.ttf]])
+LSM:Register("font", "NDS12", [[Interface\Addons\neurotech_media\media\fonts\NDS12.ttf]])
+LSM:Register("font", "Reactor7", [[Interface\Addons\neurotech_media\media\fonts\Reactor7.ttf]])
+LSM:Register("font", "Steelflight", [[Interface\Addons\neurotech_media\media\fonts\Steelflight.ttf]])
