d1bf5bb80ee11e3e466006d6b913ecdf5165bd9f
Tim Douglas <neurotech@gmail.com>
2026-02-20 09:11:22 +1100

    Added Cabin SemiBold

--- neurotech_media.lua
+++ neurotech_media.lua
@@ -5,5 +5,6 @@ if not LSM then
 else
     LSM:Register("statusbar", "!neurotech01", [[Interface\Addons\neurotech_media\media\textures\neurotech01.tga]])
     LSM:Register("font", "Cabin", [[Interface\Addons\neurotech_media\media\fonts\Cabin.ttf]])
+    LSM:Register("font", "Cabin SemiBold", [[Interface\Addons\neurotech_media\media\fonts\Cabin-SemiBold.ttf]])
     LSM:Register("font", "Cabin Bold", [[Interface\Addons\neurotech_media\media\fonts\Cabin-Bold.ttf]])
 end
