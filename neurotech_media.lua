dce66237dcd180fbc042f01994bcbef680a0c94c
Tim Douglas <neurotech@gmail.com>
2026-02-20 07:34:03 +1100

    Added Cabin Regular, renamed Cabin-Bold to 'Cabin Bold'

--- neurotech_media.lua
+++ neurotech_media.lua
@@ -4,5 +4,6 @@ if not LSM then
     return
 else
     LSM:Register("statusbar", "!neurotech01", [[Interface\Addons\neurotech_media\media\textures\neurotech01.tga]])
-    LSM:Register("font", "Cabin-Bold", [[Interface\Addons\neurotech_media\media\fonts\Cabin-Bold.ttf]])
+    LSM:Register("font", "Cabin", [[Interface\Addons\neurotech_media\media\fonts\Cabin.ttf]])
+    LSM:Register("font", "Cabin Bold", [[Interface\Addons\neurotech_media\media\fonts\Cabin-Bold.ttf]])
 end
